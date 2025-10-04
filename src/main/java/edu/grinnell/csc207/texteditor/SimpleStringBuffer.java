5278336fc17b086e10d6f4198760361867c6d8bd
Alexander <alexande3@hollerith.cs.grinnell.edu>
2025-10-03 19:43:41 -0500

    Starting the string buffer process.

--- src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
@@ -4,36 +4,64 @@ package edu.grinnell.csc207.texteditor;
  * A naive implementation of a text buffer using a <code>String</code>.
  */
 public class SimpleStringBuffer {
+    private String s;
+    private int index;
+    private int sz;
+
+    SimpleStringBuffer() {
+        this.s = s;
+        this.index = index;
+        this.sz = sz;
+    }
+
     public void insert(char ch) {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        s += " ";
+        char[] sArray = s.toCharArray();
+        sz++;
+        for (int i = (sz - 1); i > index; i--) {
+            sArray[i] = sArray[i - 1];
+        } sArray[index] = ch;
+        s = sArray.toString();
     }
 
     public void delete() {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        if (sz != 0) {
+            char[] sArray = s.toCharArray();
+            for (int i = index; i < (sz - 1); i++) {
+                sArray[i] = sArray[i + 1];
+            } s = sArray.toString();
+            s = s.substring(0, (sz - 1));
+            sz--;
+        }
     }
 
     public int getCursorPosition() {
-        throw new UnsupportedOperationException("Unimplemented method 'getCursorPosition'");
+        return index;
     }
 
     public void moveLeft() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
+        if (index > 0) {
+            index--;
+        }
     }
 
     public void moveRight() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
+        if (index < (sz - 1)) {
+            index++;
+        }
     }
 
     public int getSize() {
-        throw new UnsupportedOperationException("Unimplemented method 'getSize'");
+        return sz;
     }
 
     public char getChar(int i) {
-        throw new UnsupportedOperationException("Unimplemented method 'getChar'");
+        return s.toCharArray()[i];
     }
 
     @Override
     public String toString() {
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+        String myString = "String = " + s + ", Index = " + index + ", Size = " + sz;
+        return myString;
     }
 }
